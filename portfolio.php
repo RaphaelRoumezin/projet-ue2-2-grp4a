6530d20c63aa97d07095921c6971cc1d235fb239
Ethan Chaudat <echaudat@guardiaschool.fr>
2025-11-24 17:37:25 +0100

    v1 des cards exp

--- portfolio.php
+++ portfolio.php
@@ -165,23 +165,26 @@
         <p>Expériences professionnelles et personnelles</p>
         <div class="card-group">
             <div class="card">
-                <img src="..." class="card-img-top" alt="...">
+                <img src="images/extincteur30cm.jpg" class="card-img-top" alt="extincteur30cm">
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
+                    <p class="card-text"><small class="text-body-secondary">sous titre</small></p>
                     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                 </div>
             </div>
             <div class="card">
-                <img src="..." class="card-img-top" alt="...">
+                <img src="images/shrek.jpg" class="card-img-top" alt="shrek">
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
+                    <p class="card-text"><small class="text-body-secondary">sous titre</small></p>
                     <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
                 </div>
             </div>
             <div class="card">
-                <img src="..." class="card-img-top" alt="...">
+                <img src="images/ane.jpg" class="card-img-top" alt="ane">
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
+                    <p class="card-text"><small class="text-body-secondary">sous titre</small></p>
                     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
                 </div>
             </div>
